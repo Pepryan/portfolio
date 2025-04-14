dd265ced01717d8eb4a267cb7cb019286bab0f17
Febryan Ramadhan <febryanramadhan@gmail.com>
2025-04-14 11:25:16 +0700

    fixed contact button

--- app/page.js
+++ app/page.js
@@ -109,15 +109,7 @@ export default function Home() {
 
         {/* Contact Section */}
         <section id="contact" className="py-16 sm:py-24 mt-8 sm:mt-16">
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            viewport={{ once: true, amount: 0.1 }}
-          >
-
             <Contact />
-          </motion.div>
         </section>
       </main>
 
